a781fda332f334cac1ef66c9e0307fd251aa92bd
Annie <anniehartmann97@gmail.com>
2023-03-28 16:38:11 +0300

    Slider

---
+++ site/snippets/home/carousel.php
@@ -0,0 +1,24 @@
+<div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
+  <div class="owl-carousel header-carousel position-relative">
+    <?php foreach($page->images() as $image): ?>
+      <div class="owl-carousel-item position-relative" data-dot="<img src='<?= $image->url() ?>'>">
+        <img class="img-fluid" src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
+        <div class="owl-carousel-inner">
+          <div class="container">
+            <div class="row justify-content-start">
+              <div class="col-10 col-lg-8">
+                <h1 class="display-2 text-white animated slideInDown">
+                  <?= $image->title()->or('Empty title') ?>
+                </h1>
+                <p class="fs-5 fw-medium text-white mb-4 pb-3">
+                  <?= $image->text()->or('Empty text') ?>
+                </p>
+                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endforeach ?>
+  </div>
+</div>
--- site/templates/home.php
+++ site/templates/home.php
@@ -35,8 +35,6 @@
         </div>
     </div>
     <!-- Topbar End -->
-
-
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
         <a href="index.html" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
@@ -67,57 +65,8 @@
         </div>
     </nav>
     <!-- Navbar End -->
-
-
-    <!-- Carousel Start -->
-    <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="owl-carousel header-carousel position-relative">
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
-                <img class="img-fluid" src="img/carousel-1.jpg" alt="">
-                <div class="owl-carousel-inner">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-2.jpg'>">
-                <img class="img-fluid" src="img/carousel-2.jpg" alt="">
-                <div class="owl-carousel-inner">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-3.jpg'>">
-                <img class="img-fluid" src="img/carousel-3.jpg" alt="">
-                <div class="owl-carousel-inner">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Carousel End -->
-
+    
+    <?php snippet('home/carousel') ?>
 
     <!-- Feature Start -->
     <div class="container-xxl py-5">
