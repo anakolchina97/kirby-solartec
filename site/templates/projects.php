0798619e3764fd09c11ad1c65eb11b2cc1d3a8df
Annie <anniehartmann97@gmail.com>
2023-04-04 11:31:01 +0300

    Create snippets

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -4,14 +4,9 @@
     <?php snippet('spinner') ?>
     <?php snippet('topbar') ?>
     <?php snippet('navbar') ?>
-    <?php snippet('home/carousel') ?>
-    <?php snippet('features') ?>
-    <?php snippet('about') ?>
-    <?php snippet('services') ?>
-    <?php snippet('why') ?>
+    <?php snippet('header') ?>
     <?php snippet('projects') ?>
     <?php snippet('quote') ?>
-    <?php snippet('team') ?>
     <?php snippet('slider') ?>
     <?php snippet('footer') ?>
 </body>
