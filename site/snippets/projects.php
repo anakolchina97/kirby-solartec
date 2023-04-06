960cf92ef5066c41a886b65b0d5c7417458882b5
Annie <anniehartmann97@gmail.com>
2023-04-06 17:29:05 +0300

    Upd

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -16,7 +16,7 @@
     </div>
     <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="0.5s">
       <?php foreach ($site->find('snippets')->find('projects')->children() as $project) : ?>
-        <div class="col-lg-4 col-md-6 portfolio-item <?= $project->category() ?><">
+        <div class="col-lg-4 col-md-6 portfolio-item <?= $project->category() ?>">
           <div class="portfolio-img rounded overflow-hidden">
             <img class="img-fluid" src="<?= $project->images() ?>" alt="<?= image($project->images())->alt() ?>">
             <div class="portfolio-btn">
@@ -31,77 +31,6 @@
           </div>
         </div>
       <?php endforeach ?>
-
-      <!-- <div class="col-lg-4 col-md-6 portfolio-item second">
-        <div class="portfolio-img rounded overflow-hidden">
-          <img class="img-fluid" src="assets/img/img-600x400-5.jpg" alt="">
-          <div class="portfolio-btn">
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="assets/img/img-600x400-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
-          </div>
-        </div>
-        <div class="pt-3">
-          <p class="text-primary mb-0">Wind Turbines</p>
-          <hr class="text-primary w-25 my-2">
-          <h5 class="lh-base">We Are pioneers of solar & renewable energy industry</h5>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 portfolio-item third">
-        <div class="portfolio-img rounded overflow-hidden">
-          <img class="img-fluid" src="assets/img/img-600x400-4.jpg" alt="">
-          <div class="portfolio-btn">
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="assets/img/img-600x400-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
-          </div>
-        </div>
-        <div class="pt-3">
-          <p class="text-primary mb-0">Hydropower Plants</p>
-          <hr class="text-primary w-25 my-2">
-          <h5 class="lh-base">We Are pioneers of solar & renewable energy industry</h5>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 portfolio-item first">
-        <div class="portfolio-img rounded overflow-hidden">
-          <img class="img-fluid" src="assets/img/img-600x400-3.jpg" alt="">
-          <div class="portfolio-btn">
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="assets/img/img-600x400-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
-          </div>
-        </div>
-        <div class="pt-3">
-          <p class="text-primary mb-0">Solar Panels</p>
-          <hr class="text-primary w-25 my-2">
-          <h5 class="lh-base">We Are pioneers of solar & renewable energy industry</h5>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 portfolio-item second">
-        <div class="portfolio-img rounded overflow-hidden">
-          <img class="img-fluid" src="assets/img/img-600x400-2.jpg" alt="">
-          <div class="portfolio-btn">
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="assets/img/img-600x400-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
-          </div>
-        </div>
-        <div class="pt-3">
-          <p class="text-primary mb-0">Wind Turbines</p>
-          <hr class="text-primary w-25 my-2">
-          <h5 class="lh-base">We Are pioneers of solar & renewable energy industry</h5>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 portfolio-item third">
-        <div class="portfolio-img rounded overflow-hidden">
-          <img class="img-fluid" src="assets/img/img-600x400-1.jpg" alt="">
-          <div class="portfolio-btn">
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="assets/img/img-600x400-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
-          </div>
-        </div>
-        <div class="pt-3">
-          <p class="text-primary mb-0">Hydropower Plants</p>
-          <hr class="text-primary w-25 my-2">
-          <h5 class="lh-base">We Are pioneers of solar & renewable energy industry</h5>
-        </div>
-      </div> -->
     </div>
   </div>
 </div>
