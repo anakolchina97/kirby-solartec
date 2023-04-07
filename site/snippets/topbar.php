e0fde1adb0a6e37644a3ed7ac39d40fc70d1eca7
Annie <anniehartmann97@gmail.com>
2023-04-07 15:53:52 +0300

    Top bar

--- site/snippets/topbar.php
+++ site/snippets/topbar.php
@@ -3,23 +3,27 @@
     <div class="col-lg-7 px-5 text-start">
       <div class="h-100 d-inline-flex align-items-center me-4">
         <small class="fa fa-map-marker-alt text-primary me-2"></small>
-        <small>123 Street, New York, USA</small>
+        <small><?= $site->find('snippets')->find('topbar')->address() ?></small>
       </div>
       <div class="h-100 d-inline-flex align-items-center">
         <small class="far fa-clock text-primary me-2"></small>
-        <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
+        <small><?= $site->find('snippets')->find('topbar')->schedule() ?></small>
       </div>
     </div>
     <div class="col-lg-5 px-5 text-end">
       <div class="h-100 d-inline-flex align-items-center me-4">
         <small class="fa fa-phone-alt text-primary me-2"></small>
-        <small>+012 345 6789</small>
+        <small><?= $site->find('snippets')->find('topbar')->phone() ?></small>
       </div>
       <div class="h-100 d-inline-flex align-items-center mx-n2">
-        <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-facebook-f"></i></a>
-        <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-twitter"></i></a>
-        <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-linkedin-in"></i></a>
-        <a class="btn btn-square btn-link rounded-0" href=""><i class="fab fa-instagram"></i></a>
+        <?php
+        $numItems = count($site->find('snippets')->find('topbar')->children());
+        $i = 0;
+        ?>
+
+        <?php foreach ($site->find('snippets')->find('topbar')->children() as $topbar) : ?>
+          <a class="btn btn-square btn-link rounded-0 <?php echo ++$i !== $numItems ? 'border-0 border-end border-secondary' : ''; ?>" href="<?= $topbar->link() ?>"><i class="<?= $topbar->icon() ?>"></i></a>
+        <?php endforeach ?>
       </div>
     </div>
   </div>
