5847e972cd424d77fa6f03d43eca3c0c2ba06a3f
Annie <anniehartmann97@gmail.com>
2023-04-07 11:53:54 +0300

    Breadcrumb

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,12 +1,16 @@
-<div class="container-fluid page-header py-5 mb-5">
-    <div class="container py-5">
-        <h1 class="display-3 text-white mb-3 animated slideInDown"><?= $page->title() ?></h1>
-        <nav aria-label="breadcrumb animated slideInDown">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                <li class="breadcrumb-item text-white active" aria-current="page">Features</li>
-            </ol>
-        </nav>
-    </div>
-</div>
+ <div class="container-fluid page-header py-5 mb-5">
+   <div class="container py-5">
+     <h1 class="display-3 text-white mb-3 animated slideInDown"><?= $page->title() ?></h1>
+     <nav aria-label="breadcrumb animated slideInDown">
+       <ol class="breadcrumb">
+         <?php foreach ($site->breadcrumb() as $crumb) : ?>
+           <li class="breadcrumb-item <?= e($crumb->isActive(), 'text-white active"') ?>">
+             <a class="text-white" href="<?= $crumb->url() ?>">
+               <?= html($crumb->title()) ?>
+             </a>
+           </li>
+         <?php endforeach ?>
+       </ol>
+     </nav>
+   </div>
+ </div>
