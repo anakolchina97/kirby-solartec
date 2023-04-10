c73f3dedeedd51d19a3dfe084ea9fd0bf3bf8453
Annie <anniehartmann97@gmail.com>
2023-04-10 10:56:48 +0300

    Add features for about

--- site/snippets/features.php
+++ site/snippets/features.php
@@ -1,20 +1,37 @@
 <div class="container-xxl py-5">
   <div class="container">
     <div class="row g-5">
-      <?php foreach ($site->find('snippets')->find('features')->info()->toStructure() as $feature) : ?>
-        <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="<?= $feature->delay() ?>">
-          <div class="d-flex align-items-center mb-4">
-            <div class="btn-lg-square bg-primary rounded-circle me-3">
-              <i class="<?= $feature->icon() ?>"></i>
+      <?php if ($page->features()->isEmpty()) : ?>
+        <?php foreach ($site->find('snippets')->find('features')->features()->toStructure() as $feature) : ?>
+          <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="<?= $feature->delay() ?>">
+            <div class="d-flex align-items-center mb-4">
+              <div class="btn-lg-square bg-primary rounded-circle me-3">
+                <i class="<?= $feature->icon() ?>"></i>
+              </div>
+              <h1 class="mb-0" data-toggle="counter-up">
+                <?= $feature->count() ?>
+              </h1>
             </div>
-            <h1 class="mb-0" data-toggle="counter-up">
-              <?= $feature->count() ?>
-            </h1>
+            <h5 class="mb-3"><?= $feature->titleCard() ?></h5>
+            <span><?= $feature->text() ?></span>
           </div>
-          <h5 class="mb-3"><?= $feature->titleCard() ?></h5>
-          <span><?= $feature->text() ?></span>
-        </div>
-      <?php endforeach ?>
+        <?php endforeach ?>
+      <?php else : ?>
+        <?php foreach ($page->features()->toStructure() as $feature) : ?>
+          <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="<?= $feature->delay() ?>">
+            <div class="d-flex align-items-center mb-4">
+              <div class="btn-lg-square bg-primary rounded-circle me-3">
+                <i class="<?= $feature->icon() ?>"></i>
+              </div>
+              <h1 class="mb-0" data-toggle="counter-up">
+                <?= $feature->count() ?>
+              </h1>
+            </div>
+            <h5 class="mb-3"><?= $feature->titleCard() ?></h5>
+            <span><?= $feature->text() ?></span>
+          </div>
+        <?php endforeach ?>
+      <?php endif ?>
     </div>
   </div>
 </div>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,12 @@
  <div class="container-fluid page-header py-5 mb-5">
    <div class="container py-5">
-     <h1 class="display-3 text-white mb-3 animated slideInDown"><?= $page->title() ?></h1>
+     <h1 class="display-3 text-white mb-3 animated slideInDown">
+       <?php if ($page->titlePage()->isEmpty()) : ?>
+         <?= $page->title() ?>
+       <?php else : ?>
+         <?= $page->titlePage() ?>
+       <?php endif ?>
+     </h1>
      <nav aria-label="breadcrumb animated slideInDown">
        <ol class="breadcrumb">
          <?php foreach ($site->breadcrumb() as $crumb) : ?>
