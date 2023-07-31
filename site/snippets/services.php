fcdecc9aec99d380605ba45056bcecd2f7578139
Annie <anniehartmann97@gmail.com>
2023-07-31 12:41:18 +0300

    services

--- site/snippets/services.php
+++ site/snippets/services.php
@@ -1,8 +1,20 @@
 <div class="container-xxl py-5">
   <div class="container">
     <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-      <h6 class="text-primary"><?= $site->find('snippets')->find('services')->subtitle() ?></h6>
-      <h1 class="mb-4"><?= $site->find('snippets')->find('services')->titleText() ?></h1>
+      <h6 class="text-primary">
+        <?php if ($page->subtitleServices()->isEmpty()) : ?>
+          <?= $site->find('snippets')->find('services')->subtitle() ?>
+        <?php else : ?>
+          <?= $page->subtitleServices() ?>
+        <?php endif ?>
+      </h6>
+      <h1 class="mb-4">
+        <?php if ($page->titleServices()->isEmpty()) : ?>
+          <?= $site->find('snippets')->find('services')->titleText() ?>
+        <?php else : ?>
+          <?= $page->titleServices() ?>
+        <?php endif ?>
+      </h1>
     </div>
     <div class="row g-4">
       <?php if ($page->services()->isEmpty()) : ?>
@@ -25,7 +37,9 @@
         <?php foreach ($page->services()->toStructure() as $service) : ?>
           <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="<?= $service->delay() ?>">
             <div class="service-item rounded overflow-hidden">
-              <img class="img-fluid" src="<?= $service->img() ?>" alt="">
+              <?php if ($image = $service->images()->toFile()) : ?>
+                <img class="img-fluid" src="<?= $image->url() ?>" alt="">
+              <?php endif ?>
               <div class="position-relative p-4 pt-0">
                 <div class="service-icon">
                   <i class="<?= $service->icon() ?>"></i>
